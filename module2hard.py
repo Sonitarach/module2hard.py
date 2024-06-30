1f5646412aea6c606bc5098a87429117131c73f2
Sonitarach <sofar07@bk.ru>
2024-06-30 15:12:29 +0500

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,14 @@
+#Древний шифр
+def find_password(n):
+    pairs = []
+    for i in range(1, 21):
+        for j in range(i + 1, 21):
+            if n % (i + j) == 0:
+                pairs.append(f"{i}{j}")
+                result = ''.join(pairs)
+    return result
+
+for n in range(3, 21):
+    print(f"{n} - {find_password(n)}")
+
+
